9a18f46f96fb9166b36920e672544f537a1ab038
kkznch <kkznch710md@gmail.com>
2020-09-26 14:03:06 +0900

    [update]コードフォーマットの設定した

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,34 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    '@react-native-community',
+    'plugin:react-native/all',
+    'plugin:jest/recommended',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'jest'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'no-console': 'warn',
+    yoda: 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-native/sort-styles': 'off',
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,44 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  parser: 'babel',
+  filepath: '',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
   singleQuote: true,
+  quoteProps: 'as-needed',
   trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  requirePragma: false,
+  insertPragma: false,
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: ['*.yml', '*.yaml'],
+      options: {
+        parser: 'yaml',
+      },
+    },
+  ],
 };
